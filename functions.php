5c907b44ae13245056af2073b75a0af5977f0d5d
James Winfield <hello@iwillbeawebdeveloper.co.uk>
2023-02-09 15:12:31 +0000

    remove print

--- functions.php
+++ functions.php
@@ -167,8 +167,6 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 
 add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
 
-print_r(get_template_directory_uri()); die();
-
 function enqueue_parent_styles() {
    wp_enqueue_style( 'readingweather', get_template_directory_uri().'/style.css' );
 }
