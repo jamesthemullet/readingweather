105be57f746ec3875950a7226d14c1e8395fc658
James Winfield <hello@iwillbeawebdeveloper.co.uk>
2025-02-22 11:18:12 +0000

    basic meta

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,10 +4,17 @@
 	let { data }: PageProps = $props();
 </script>
 
+<svelte:head>
+	<title>{data.meta.title}</title>
+	<meta name="description" content={data.meta.description} />
+	<meta property="og:title" content={data.meta.title} />
+	<meta property="og:description" content={data.meta.description} />
+</svelte:head>
+
 <h1>Weather Forecast For Reading & Berkshire</h1>
 
 <ul>
-	{#each data.posts.nodes as post}
+	{#each data.posts.posts.nodes as post}
 		<li>
 			<a href="/blog/{post.slug}">
 				{post.title}
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,1 +1,12 @@
+<svelte:head>
+	<title>About Weather Forecast For Reading & Berkshire</title>
+	<meta name="description" content="Learn more about Weather Forecast For Reading & Berkshire." />
+	<meta property="og:title" content="About Weather Forecast For Reading & Berkshire" />
+	<meta
+		property="og:description"
+		content="Learn more about Weather Forecast For Reading & Berkshire."
+	/>
+	<!-- <meta property="og:image" content="/default-image.png" /> -->
+</svelte:head>
+
 <p>abbbbout</p>
