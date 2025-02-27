c3d5b4764947aaac1cf429f55a8f7ba8f4887fa8
James Winfield <hello@iwillbeawebdeveloper.co.uk>
2025-02-27 14:39:52 +0000

    index page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,8 @@
 	import type { PageProps } from './$types';
 
 	let { data }: PageProps = $props();
+
+	import '../styles/index.css';
 </script>
 
 <svelte:head>
@@ -17,8 +19,18 @@
 	{#each data.posts.posts.nodes as post}
 		<li>
 			<a href="/blog/{post.slug}">
-				{post.title}
+				{#if post.featuredImage?.node?.sourceUrl}
+					<img
+						src={post.featuredImage.node.sourceUrl}
+						srcset={post.featuredImage.node.srcSet}
+						alt={post.title}
+						width="200"
+						height="auto"
+					/>
+				{/if}
+				<h2>{post.title}</h2>
 			</a>
 		</li>
+		<div class="content">{@html post.content}</div>
 	{/each}
 </ul>
