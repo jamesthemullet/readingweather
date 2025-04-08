9bd06591bc9d842b0e12ac35a0788cd638eceb06
James Winfield <hello@iwillbeawebdeveloper.co.uk>
2025-04-08 14:29:05 +0100

    add linting job

--- src/lib/components/AddComment.svelte
+++ src/lib/components/AddComment.svelte
@@ -3,7 +3,7 @@
 	import { showAddComment } from '$lib/stores/commentState';
 	import { tick } from 'svelte';
 
-	const { postId, parentCommentId = null } = $props();
+	let { postId, parentCommentId = null } = $props();
 
 	let name = $state('');
 	let email = $state('');
