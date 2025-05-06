2af7ff59868b61da4d36645ac414476375fed7e3
James Winfield <hello@jameswinfield.co.uk>
2025-05-06 18:39:05 +0100

    proper way for svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,10 @@
 <script>
 	import '../styles/global.css';
 	import NavBar from '$lib/components/NavBar.svelte';
+	import Analytics from '$lib/analytics/analytics.svelte';
 </script>
 
+<Analytics />
 <NavBar />
 <main>
 	<slot />
